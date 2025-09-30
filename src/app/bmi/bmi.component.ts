e3133363cf813334dcb75c3976f5d30a52923248
barte28 <bartek.teuerle@gmail.com>
2025-09-30 09:46:19 +0200

    koniec

--- src/app/bmi/bmi.component.ts
+++ src/app/bmi/bmi.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-bmi',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './bmi.component.html',
   styleUrl: './bmi.component.css'
 })
 export class BmiComponent {
+  waga = 0;
+  wzrost = 0;
+  bmi = 0;
 
+  oblicz(){
+    this.bmi = (Math.round(this.waga/(this.wzrost * this.wzrost) *100))/100
+  }
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+    plec = ""
+    bialko = true
+    nabial = true
+    warzywa= true
+    wiek = ""
 
+    pokazparagraf = false
+
+    pokaz(){
+      
+      if(this.pokazparagraf == true){
+        this.pokazparagraf = false
+      }
+      else{
+        this.pokazparagraf = true
+      }
+    }
 }
